2b71538b02c0524a8e21bbd23e152dd41b606e3a
Sebastián Amigón <seb.fxndx1@gmail.com>
2024-05-30 20:42:14 -0600

    rama array

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const date = new Date()
 const nombre = 'Sebastian'
 console.log(`Hola ${nombre}, la fecha de hoy es ${date}`)
+
+let mangas = [
+    'Jojos Bizzare Adventure',
+    'Jujutsu kaisen',
+    'Katekyo Hitman Reborn'
+]
+
+mangas.push('Mashle')
